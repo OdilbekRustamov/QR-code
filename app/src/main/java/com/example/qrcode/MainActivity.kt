434a819b073a3b216ac3fc0280c4aadf497d3c41
Rustamov Odilbek <odilbekrustamov1215@gmail.com>
2022-08-03 10:01:51 +0300

    finish!!

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -3,20 +3,44 @@ package com.example.qrcode
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.*
+import androidx.annotation.RequiresApi
+import com.example.qrcode.Asymmetric.Companion.decryptMessage
+import com.example.qrcode.Asymmetric.Companion.encryptMessage
 import com.google.zxing.integration.android.IntentIntegrator
 import net.glxn.qrgen.android.QRCode
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var privateKey = BuildConfig.PRIVATE_KEY
+    var publicKey = BuildConfig.PUBLIC_KEY
+
+    private val TAG = "MainActivity"
+//    val keyPairGenerator = Asymmetric()
+//    // Generate private and public key
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    val privateKey: String = Base64.getEncoder().encodeToString(keyPairGenerator.privateKey.encoded)
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    val publicKey: String = Base64.getEncoder().encodeToString(keyPairGenerator.publicKey.encoded)
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("MainActivity", privateKey)
+        Log.d("MainActivity", publicKey)
+
         setupUI()
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setupUI() {
         val qr_code_image = findViewById<ImageView>(R.id.qr_code)
         val qr_code_text = findViewById<EditText>(R.id.qr_text)
@@ -24,8 +48,13 @@ class MainActivity : AppCompatActivity() {
         val scanerBtn = findViewById<Button>(R.id.scaner_btn)
 
         qr_code_button.setOnClickListener {
-            val myBitmap: Bitmap = QRCode.from(qr_code_text.text.toString()).bitmap()
-            qr_code_image.setImageBitmap(myBitmap)
+            val text = qr_code_text.text.toString()
+            if (text.isNotEmpty()){
+                val myBitmap: Bitmap = QRCode.from(testAsymmetricEncrypt(text)).bitmap()
+                qr_code_image.setImageBitmap(myBitmap)
+            }else{
+                Toast.makeText(this, "Oldin Text kiriting", Toast.LENGTH_SHORT).show()
+            }
         }
 
         scanerBtn.setOnClickListener {
@@ -37,16 +66,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            val text = testAsymmetricDecrypted(result.contents)
             if (result!=null){
-                findViewById<TextView>(R.id.tv_set_text).text = result.contents
-                Toast(this).showCustomToast(result.contents, this)
+                findViewById<TextView>(R.id.tv_set_text).text = text
+                Toast(this).showCustomToast(text, this)
             }
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun testAsymmetricDecrypted(encryptedValue: String): String {
+        // Decrypt
+        val decryptedText = decryptMessage(encryptedValue, privateKey)
+        Log.d(TAG, "Decrypted output: $decryptedText")
+        return decryptedText
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun testAsymmetricEncrypt(secretText: String): String{
+        // Encrypt secret text using public key
+        val encryptedValue = encryptMessage(secretText, publicKey)
+        Log.d(TAG, "Encrypted Value: $encryptedValue")
+        return encryptedValue
+    }
+
+
 }
 
 fun Toast.showCustomToast(massage: String, activity: Activity) {
